3daf93fee6f21c1c62801c8cd6b1978d9deeb8ea
me <ogogan45@gmail.com>
2022-09-30 18:05:49 +0300

    quiz-app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import questions from './data/data';
 
 function App() {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+  const [score, setScore] = useState(0);
+
+  const resetButtonHandler = () => {
+    setShowScore(false);
+    setCurrentQuestion(0);
+    setScore(0);
+  };
+
+  const onClickAnswerHandler = (isCorrect) => {
+    const nextQuestion = currentQuestion + 1,
+      increaseScore = score + 1;
+
+    if (isCorrect === true) {
+      setScore(increaseScore);
+    }
+
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      setShowScore(true);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {showScore ? (
+        <div className="scoreEnd">
+          <div className="scoreection">
+            You scored {score} out of {questions.length}
+          </div>
+          <div className="resetButton">
+            <button onClick={resetButtonHandler}> RESET</button>
+          </div>
+        </div>
+      ) : (
+        <>
+          <div className="questionSelection">
+            <div className="questionCount">
+              <span>Question {currentQuestion + 1}</span>/{questions.length}
+            </div>
+            <div className="questionText">
+              {questions[currentQuestion].questionText}
+            </div>
+          </div>
+          <div className="answerSelection">
+            {questions[currentQuestion].answerOptions.map((question, i) => {
+              return (
+                <button
+                  onClick={() => onClickAnswerHandler(question.isCorrect)}
+                  key={i}
+                >
+                  {question.answerText}
+                </button>
+              );
+            })}
+          </div>
+        </>
+      )}
     </div>
   );
 }
---
+++ src/data/data.js
@@ -0,0 +1,31 @@
+const questions = [
+  {
+    questionText: 'What is the capital of France',
+    answerOptions: [
+      { answerText: 'New York', isCorrect: false },
+      { answerText: 'London', isCorrect: false },
+      { answerText: 'Paris', isCorrect: true },
+      { answerText: 'Kyiv', isCorrect: false },
+    ],
+  },
+  {
+    questionText: 'What is the capital of United Kingdom',
+    answerOptions: [
+      { answerText: 'Paris', isCorrect: false },
+      { answerText: 'New York', isCorrect: false },
+      { answerText: 'Kyiv', isCorrect: false },
+      { answerText: 'London', isCorrect: true },
+    ],
+  },
+  {
+    questionText: 'What is the capital of Ukraine',
+    answerOptions: [
+      { answerText: 'New York', isCorrect: false },
+      { answerText: 'Kyiv', isCorrect: true },
+      { answerText: 'London', isCorrect: false },
+      { answerText: 'Paris', isCorrect: false },
+    ],
+  },
+];
+
+export default questions;
